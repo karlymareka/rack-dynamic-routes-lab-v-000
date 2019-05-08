4f1cfa4df42f9657c3be61080b8efc7730b793dc
Karly Mareka <karly.mareka@gmail.com>
2019-05-08 20:41:12 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -4,7 +4,9 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
     
-    if req.path.match(/items/)
+    if req.path.includes?("/items/")
+    else
+      
   end 
   
 end 
