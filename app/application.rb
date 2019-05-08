15fda8e0d07f2a1df0a7cd383ebe1cea7b5cc354
Karly Mareka <karly.mareka@gmail.com>
2019-05-08 21:12:33 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -9,7 +9,7 @@ class Application
       search_item = req.path[7..-1]
       item_names = Item.all.collect {|item| item.name}
       if item_names.include?(search_item)
-        Item.all.find {|item| item == search_item}
+        Item.all.find {|item| item.name == search_item}
         binding.pry 
       else 
         resp.write "Item not found"
